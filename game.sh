99aaa4603d1d5bed21216d5693284199491baa2a
Kevin <kevinkhatae@gmail.com>
2025-04-25 15:08:12 -0500

    added spell and the ability to attack the GiantBat

--- game.sh
+++ game.sh
@@ -1,22 +1,53 @@
+stats
+declare -A GiantBat
+GiantBat[hp]=10
+GiantBat[attack]=2
+
 echo "Welcome to the Ultimate Adventure Game!"
 echo "Choose a class."
-read class
-if [ "$class" == "mage" ]; then
+echo "You can choose from one of these 3 classes: mage, fighter and rogue. Type in your choice."
+read class  
+
+if [ "$class" = "mage" ]; then
   cat mage.txt
-elif [ "$class" == "fighter" ];then
+elif [ "$class" = "fighter" ]; then
   cat fighter.txt
-elif [ "$class" == "rogue" ];then
+elif [ "$class" = "rogue" ]; then
   cat rogue.txt
-echo "Now choose a location to explore: forest, castle or cave."
+else
+  echo "Invalid class."
+fi
+
+echo "Now choose a location to explore: forest, castle or cave. Type in your choice"
 read location
-if [ "$location" == "forest" ]; then 
+
+if [ "$location" = "forest" ]; then 
   cat forest.txt
-elif ["$location" == "castle"]; then
+
+elif [ "$location" = "castle" ]; then   
   cat castle.txt
-elif ["$location" == "cave"]; then
+
+elif [ "$location" = "cave" ]; then    
   cat cave.txt
   echo "But wait..."
   cat monster.txt
+  
+  if [ "$class" = "mage" ]; then
+    echo 'you have 1 spell, "basic wand attack" - it does 5 damage.' 
+    echo "Type 'basic wand attack' to attack the GiantBat:"
+    read spell
+    
+    if  [ "$spell" = "basic wand attack" ]; then
+      GiantBat[hp]=$((GiantBat[hp]-5))
+      echo "The GiantBat has ${GiantBat[hp]} HP left!"
+    else 
+      echo "That spell didn't work!"
+    fi
+  fi
+    
 else
-  echo "invalid location."
+  echo "Invalid location."
 fi
+
+
+
